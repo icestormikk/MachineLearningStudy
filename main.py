fc3d1c16479b813c20a80424c247ee8d0a01abdb
Pavel Zhigalov <jigalow.pavel2015@yandex.ru>
2024-07-21 00:47:12 +0300

    Добавил ещё немного учебного кода

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import numpy as np
+from numpy_pandas.pandas.pandas_functions import pandas_test
 
 if __name__ == '__main__':
-    x0 = [[1,2],[2,3],[3,4],[4,5]]
-    x = np.array([[1, 2, 3], [2, 3, 4], [3, 4, 5], [4, 5, 6], [5, 6, 7], [6, 7, 8]])
-    y = np.transpose(x)
-    z = x @ y
-    print(x0)
-    print(x[(0, 1)])
-    print(y)
-    print(x < y)
+    pandas_test()
--- numpy_pandas/pandas/pandas_functions.py
+++ numpy_pandas/pandas/pandas_functions.py
@@ -1,7 +1,47 @@
+import os.path
 from typing import Any
 
-import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from numpy_pandas.numpy.numpy_functions import get_file_by_url
 
 
-def pandas_test(txt_filepath: str) -> Any:
-    return np.genfromtxt(txt_filepath, dtype=float, skip_header=1, delimiter=",")
+def read_csv_file(csv_filepath: str) -> Any:
+    try:
+        return pd.read_csv(csv_filepath)
+    except Exception as e:
+        print(f"Error while reading: {e}")
+        return None
+
+
+def pandas_test():
+    name = 'italy-covid-daywise.csv'
+    if not os.path.isfile(f"./{name}"):
+        name = 'italy-covid-daywise.csv'
+        get_file_by_url(
+            "https://gist.githubusercontent.com/aakashns/f6a004fa20c84fec53262f9a8bfee775/raw"
+            "/f309558b1cf5103424cef58e2ecb8704dcd4d74c/italy-covid-daywise.csv",
+            name
+        )
+
+    df = read_csv_file(f"./{name}")
+    # выбор n-го количества случайных строк
+    print(df.sample(5))
+    # суммирование данных в конкретном столбце
+    print(df.new_cases.sum())
+    # фильтрация данных из набора
+    print(df[df.new_tests > 30000])
+    # сортировка данных
+    print(df.sort_values('date', ascending=False))
+    # преобразование данных и группировка
+    df['date'] = pd.to_datetime(df['date'])
+    df['year'] = pd.DatetimeIndex(df['date']).year
+    df['month'] = pd.DatetimeIndex(df['date']).month
+    df['day'] = pd.DatetimeIndex(df['date']).day
+    print(df)
+    print(df.groupby('year')[['new_cases']].sum())
+
+    # вывод на экран графика с данными
+    df['new_deaths'].cumsum().plot(title='Total Deaths')
+    plt.show()
